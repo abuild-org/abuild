626f04e6a74357fda1b8deb43f791936ceae7828
Jay Berkenbilt <ejb@ql.org>
2009-03-12 20:48:34 +0000

    QTestSupport -> qtest
    git-svn-id: file:///home/ejb/tmp/z/abuild.svn/trunk@1032 08200017-a7fb-0310-af7e-98bf598cde68

--- groovy/qtest.groovy
+++ groovy/qtest.groovy
@@ -26,7 +26,7 @@ abuild.addTargetClosure('test-only') {
                     'value':12)
             }
             def toExport = abuild.resolveAsList('qtest.export', [])
-            if (abuild.resolve('TESTS'))
+            if (abuild.resolve('TESTS') != null)
             {
                 toExport << 'TESTS'
             }
