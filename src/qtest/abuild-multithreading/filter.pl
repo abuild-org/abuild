443584321c17d51c167d79e541bd0c0ddd708571
Jay Berkenbilt <ejb@ql.org>
2009-04-22 21:10:21 +0000

    save failures in multithreading test
    git-svn-id: file:///home/ejb/tmp/z/abuild.svn/trunk@1347 08200017-a7fb-0310-af7e-98bf598cde68

--- src/qtest/abuild-multithreading/filter.pl
+++ src/qtest/abuild-multithreading/filter.pl
@@ -5,22 +5,28 @@ my %attempted = ();
 my %failed = ();
 
 my $show_failures = 1;
+my $logfile = shift(@ARGV);
 if (@ARGV && ($ARGV[0] = '-no-failures'))
 {
     $show_failures = 0;
 }
 
+open(L, ">$logfile");
 while (<STDIN>)
 {
-    if (m/^abuild: (\S+) \(abuild-.*?\): all/)
+    print L;
+    # These regexps are not anchored since they are sometimes
+    # interleaved with make output.
+    if (m/abuild: (\S+) \(abuild-.*?\): all/)
     {
 	$attempted{$1} = 1;
     }
-    elsif (m/^abuild: (\S+) \(abuild-.*?\): build failed/)
+    elsif (m/abuild: (\S+) \(abuild-.*?\): build failed/)
     {
 	$failed{$1} = 1;
     }
 }
+close(L);
 
 print "attempted: ", join(' ', sort keys %attempted), "\n";
 print "failed: ", join(' ', sort keys %failed), "\n" if $show_failures;
