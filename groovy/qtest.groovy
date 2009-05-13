a94229890608e3a25e2c618b7d405de489baca36
Jay Berkenbilt <ejb@ql.org>
2009-05-13 18:12:55 +0000

    move logic of invoking qtest into a custom task, call custom task from both ant and groovy backends; makes shell qtest-driver wrappers work in windows
    git-svn-id: file:///home/ejb/tmp/z/abuild.svn/trunk@1438 08200017-a7fb-0310-af7e-98bf598cde68

--- groovy/qtest.groovy
+++ groovy/qtest.groovy
@@ -1,4 +1,7 @@
 import org.abuild.groovy.Util
+import org.abuild.ant.QTest
+
+ant.taskdef('name': 'qtest', 'classname': 'org.abuild.ant.QTest')
 
 abuild.addTargetDependencies('test-only', ['test-qtest'])
 abuild.addTargetClosure('test-qtest') {
@@ -6,89 +9,28 @@ abuild.addTargetClosure('test-qtest') {
     def qtest = new File("${src}/qtest")
     if (qtest.isDirectory())
     {
-        def build = abuild.buildDirectory.path
         def tty = abuild.interfaceVars['ABUILD_STDOUT_IS_TTY']
-        def command = 'qtest-driver'
-        def args = ['-datadir', '../qtest',
-            '-bindirs', '..:.', '-covdir', '..',
-            "-stdout-tty=${tty}"]
-        if (Util.inWindows)
-        {
-            // Find qtest-driver in path and figure out how to invoke
-            // it.  qtest requires cygwin perl, so it won't work
-            // without cygwin in the path anyway.  Look for either
-            // something that starts with #!/.../perl or #!/.../sh,
-            // which could be a wrapper script.
-            def pathSep = ant.project.properties['path.separator']
-            def path = System.env['PATH'].split(pathSep)
-            String interpreter
-            String driver
-            for (p in path)
+        def toExport = abuild.resolveAsList('qtest.export', [])
+        ['TESTS', 'TC_SRCS'].each {
+            if (abuild.resolve(it) != null)
             {
-                def candidate = new File(p, 'qtest-driver')
-                if (candidate.isFile())
-                {
-                    candidate.withReader {
-                        reader ->
-                        def firstline = reader.readLine()
-                        def m = (firstline =~ /#!(\S+)/)
-                        if (m)
-                        {
-                            interpreter = new File(m.group(1)).name
-                            if (interpreter == 'env')
-                            {
-                                m = firstline =~ /env (\S+)/
-                                if (m)
-                                {
-                                    interpreter = m.group(1)
-                                }
-                            }
-                            driver = candidate.absolutePath
-                        }
-                    }
-                }
-                if ((interpreter == 'perl') || (interpreter == 'sh'))
-                {
-                    break
-                }
-            }
-
-            // Perl, especially cygwin perl, needs /, not \ in a path.
-            driver = driver.replaceAll('\\\\', '/');
-            if (interpreter)
-            {
-                command = interpreter
-                args = [driver, *args]
+                toExport << it
             }
         }
 
-        ant.exec('failonerror':'true', 'executable': command,
-                 'dir': build)
+        def qtestAttrs = [
+            'stdoutistty': (tty == "1"),
+            'emacsmode': abuild.buildArgs.emacsMode
+        ]
+        if (toExport)
         {
-            if (! abuild.buildArgs.emacsMode)
-            {
-                env('key':'QTEST_EXTRA_MARGIN',
-                    'value':12)
-            }
-            def toExport = abuild.resolveAsList('qtest.export', [])
-            ['TESTS', 'TC_SRCS'].each {
-                if (abuild.resolve(it) != null)
-                {
-                    toExport << it
-                }
-            }
-            if (toExport)
-            {
+            qtestAttrs['environment'] = toExport.collect {
                 // Windows wants environment variables to be all
                 // upper-case.
-                toExport.each {
-                    env('key': it.toUpperCase(),
-                        'value': abuild.resolveAsList(it).join(' '))
-                }
-            }
-            args.each {
-                arg('value':it)
-            }
+                it.toUpperCase() + '=' + abuild.resolveAsList(it).join(' ')
+            }.join('\000')
         }
+
+        ant.qtest(qtestAttrs)
     }
 }
