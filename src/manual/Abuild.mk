1e47f1d35a2d82fa4a554dcff410394f5b965624
Jay Berkenbilt <ejb@ql.org>
2009-06-12 16:29:40 +0000

    remove dtds from dependency list
    git-svn-id: file:///home/ejb/tmp/z/abuild.svn/trunk@1468 08200017-a7fb-0310-af7e-98bf598cde68

--- src/manual/Abuild.mk
+++ src/manual/Abuild.mk
@@ -3,5 +3,5 @@ MAIN_DOC_OUTPUT_PREFIX := abuild-
 
 FIGURE_SRCS := $(wildcard $(SRCDIR)/figures/*.dia)
 FIGURES := $(foreach F,$(FIGURE_SRCS),$(patsubst %.dia,%.png,$(notdir $(F))))
-EXTRA_DEPS := $(SRCDIR)/../abuild_data.dtd $(FIGURES) stylesheet.css
+EXTRA_DEPS := $(FIGURES) stylesheet.css
 LOCAL_RULES := docbook.mk figures.mk extrafiles.mk
