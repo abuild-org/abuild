c69caaa588c22a33f9cb5b5492575d3b86d8d1df
Jay Berkenbilt <ejb@ql.org>
2010-02-26 23:34:19 +0000

    includeAntRuntime
    git-svn-id: file:///home/ejb/tmp/z/abuild.svn/trunk@1603 08200017-a7fb-0310-af7e-98bf598cde68

--- src/java-support/Abuild.groovy
+++ src/java-support/Abuild.groovy
@@ -2,4 +2,5 @@ parameters {
     abuild.rules = ['java', 'groovy']
     java.jarName = 'abuild-java-support.jar'
     java.compile = ['source': '1.5', 'target': '1.5']
+    java.includeAntRuntime = 'true'
 }
