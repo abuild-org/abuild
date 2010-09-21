dbe37cea06fb406fc440b98a4eb5881de826524a
Jay Berkenbilt <ejb@ql.org>
2010-09-21 15:41:09 +0000

    comment
    git-svn-id: file:///home/ejb/tmp/z/abuild.svn/trunk@1696 08200017-a7fb-0310-af7e-98bf598cde68

--- src/local-rules.mk
+++ src/local-rules.mk
@@ -1,6 +1,8 @@
 .PRECIOUS: interface.fl
 
-# Generate .fl from .qfl file
+# Generate .fl from .qfl file.  For some reason, using a pattern rule
+# here doesn't work under cygwin, though it does work under Linux with
+# make 3.81.
 interface.fl: interface.qfl $(SRCDIR)/gen_flex interface.yy
 	@$(PRINT) Generating $@ from $<
 	perl $(SRCDIR)/gen_flex $< $@
