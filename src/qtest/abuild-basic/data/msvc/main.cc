7e3d601717975fb6fb490794002ebf95b0a9bae3
Jay Berkenbilt <ejb@ql.org>
2010-11-05 14:43:26 +0000

    adjust msvc tests to work with newer versions of msvc
    git-svn-id: file:///home/ejb/tmp/z/abuild.svn/trunk@1745 08200017-a7fb-0310-af7e-98bf598cde68

--- src/qtest/abuild-basic/data/msvc/main.cc
+++ src/qtest/abuild-basic/data/msvc/main.cc
@@ -1,9 +1,21 @@
 #include <iostream>
+#include <cstring>
 
-int main()
+int main(int argc, char* argv[])
 {
+    if ((argc == 2) && (std::strcmp(argv[1], "ver") == 0))
+    {
+	std::cout << _MSC_VER << std::endl;
+	return 0;
+    }
+
     try
     {
+#ifdef _MANAGED
+	std::cout << "managed" << std::endl;
+#else
+	std::cout << "not managed" << std::endl;
+#endif
 	throw 5;
     }
     catch (int x)
