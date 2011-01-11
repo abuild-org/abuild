4527a09a2ae28e803eeffb936aa922ed6e986738
Jay Berkenbilt <ejb@ql.org>
2011-01-11 22:51:28 +0000

    update comment
    git-svn-id: file:///home/ejb/tmp/z/abuild.svn/trunk@1765 08200017-a7fb-0310-af7e-98bf598cde68

--- src/qtest/abuild-output-capture/data/t1/f/local.groovy
+++ src/qtest/abuild-output-capture/data/t1/f/local.groovy
@@ -3,9 +3,10 @@ abuild.addTargetClosure('all') {
     System.out.flush()
     if (abuild.resolve('MISBEHAVE'))
     {
-        // For some reason, passing the closure by putting after the
-        // method call rather than explicit passing it works in groovy
-        // 1.6.7 but not in 1.7.6.
+        // Passing the closure by putting after the method call rather
+        // than explicitly passing it no longer works in groovy >= 1.7
+        // for Thread because there's no way to distinguish the
+        // closure from an anonymous inner class.
         def t = new Thread(new ThreadGroup(), {
             // Print something that doesn't end in a newline and do it
             // from a separate thread group so abuild won't be able to
